bc2cc5d2ecc4fe72d018a3485acd61f34cf04bfa
Thomas Dubois <thomasdubois085@gmail.com>
2017-10-20 09:18:32 +0200

    Systeme de points, de collision et animations diverses

--- src/three/index.js
+++ src/three/index.js
@@ -1,7 +1,11 @@
 import * as THREE from 'three';
 import TweenMax from 'gsap';
-import threeOrbitControls from './utils/OrbitControls';
-import Stats from 'stats.js';
+import 'three/examples/js/controls/OrbitControls';
+import 'three/examples/js/postprocessing/EffectComposer';
+import 'three/examples/js/postprocessing/RenderPass';
+import 'three/examples/js/postprocessing/ShaderPass';
+import 'three/examples/js/shaders/CopyShader';
+import 'three/examples/js/shaders/FilmShader';
 import Game from './game';
 import Table from './table';
 import Lights from './lights';
@@ -9,11 +13,11 @@ import Particles from './particles';
 import './index.css';
 
 // attach orbit controls to THREE
-const OrbitControls = threeOrbitControls(THREE);
+// const OrbitControls = threeOrbitControls(THREE);
 
 // stats
-const stats = new Stats();
-document.body.appendChild(stats.domElement);
+// const stats = new Stats();
+// document.body.appendChild(stats.domElement);
 
 // scene, renderer, camera, mesh (geometry + material)
 const renderer = new THREE.WebGLRenderer({
@@ -23,6 +27,24 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 // enbale the drawing of shadows
 renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
+
+let body =  document.querySelector('body');
+body.style.display = "block";
+
+let scoreJ1 =  document.getElementById('j1').innerHTML;
+let scoreJ2 =  document.getElementById('j2').innerHTML;
+let indicationJ1 =  document.getElementById('indicationJ1').innerHTML;
+let indicationJ2 =  document.getElementById('indicationJ2').innerHTML;
+const modal = document.getElementById('myModal');
+const resultat = document.querySelector('.resultat');
+const panel = document.querySelectorAll('.score');
+const span = document.getElementsByClassName("close")[0];
+span.onclick = () => {
+    modal.style.display = "none";
+}
+
+
+
 
 //Scene
 const scene = new THREE.Scene();
@@ -36,7 +58,7 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 camera.position.set(-15, 30, 95);
 
 // controls
-const controls = new OrbitControls(camera, renderer.domElement);
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 const lights = Lights();
 scene.add(lights.group);
@@ -55,17 +77,55 @@ const particlesJ2 = Particles();
 particlesJ2.group.position.set(-2.5, 7, -game.z-2.5);
 scene.add(particlesJ2.group);
 
-const speed = game.z*2;
-const angle = 0;
 let direction= '-';
-let yDestination = 3;
+let yDestination = 2;
 let win = false;
 let touchable=true;
 
+// const timer = () => {
+//     let seconds = 5;
+//     const interval = setInterval(() => {
+//             seconds --;
+//             if (seconds >=0) {
+//                 document.getElementById('rebours').innerHTML = seconds;
+//             }else {
+//                 console.log(game.balle.position.z);
+//                 direction == '-' ? direction = '+' : direction = '-';
+//                 game.balle.position.z <0 ? game.balle.position.z = game.z : game.balle.position.z = -game.z;
+//                 seconds=5;
+//                 timer();
+//             }
+//     }, 1000);
+// }
+
+
+// const speed = game.z*2;
+
+const randomAngle = (min,max) => {
+
+    return Math.floor(Math.random() * (max-min)) + min;
+    // return Math.floor(Math.random()*(max-min+1)+min);
+
+}
+
+const randomSpeed = (min,max) => {
+
+    // return Math.floor(Math.random() * (max+min)) - min;
+    return Math.floor(Math.random()*(max-min+1)+min);
+
+}
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
 
 const handleClick = e => {
+
+
+    // const speed = randomSpeed(75,150);
+    const speed = randomSpeed(120,160);
+    const angle = randomAngle(-8,8);
+    // const angle = randomAngle(-40,40);
+    // const speed = 140;
+    // const angle = 0;
 
     e.preventDefault();
     mouse.x = e.clientX / window.innerWidth * 2 - 1;
@@ -75,20 +135,33 @@ const handleClick = e => {
 
     if (intersects.length > 0 && intersects[0].object.uuid === game.balle.uuid && touchable==true) {
 
-        if (angle == 0 && speed == game.z*2){
-            yDestination = 8;
+        console.log(angle);
+        console.log(speed);
+        if (angle >= -4 && angle <=4 && speed >= game.z*2-10 && speed <= game.z*2+10){
+            // yDestination = 8;
             win = true;
+            TweenMax.to(game.balle.position, 0.9, {
+                x: '0',
+                z: `${direction}=${game.z*2}`,
+            });
+            TweenMax.to(game.balle.position, 0.6, {
+                y: 8,
+                ease: Power2.easeIn,
+            }).delay(0.3);
+        }else {
+            win = false;
+            TweenMax.to(game.balle.position, 0.9, {
+                x: angle,
+                z: `${direction}=${speed}`,
+            });
+            TweenMax.to(game.balle.position, 0.6, {
+                y: '2',
+                ease: Power2.easeIn,
+            }).delay(0.3);
         }
 
-        TweenMax.to(game.balle.position, 0.9, {
-            x: angle,
-            z: `${direction}=${speed}`,
-        });
 
-        TweenMax.to(game.balle.position, 0.6, {
-            y: yDestination,
-            ease: Power2.easeIn,
-        }).delay(0.3);
+
 
         touchable = false;
 
@@ -96,23 +169,41 @@ const handleClick = e => {
 
             if (win==true && game.balle.position.z < 0) {
 
-                // let i = 0;
-                // const interval = setInterval(() => {
-                //     if (i <= 1000) {
-                //         particlesJ2.particles[i].fart();
-                //         i++;
-                //     } else clearInterval(interval);
-                // }, 10);
+                document.getElementById('resultat').innerHTML = "Dans le mille !";
+                document.getElementById("imgResultat").src="img/best.png";
+                resultat.style.display = "block";
+
+                const timeout2 = setTimeout(() => {
+                    document.getElementById('resultat').innerHTML = "";
+                    resultat.style.display = "none";
+                }, 2500);
+
+                scoreJ1++;
+                if (scoreJ1 == 5) modal.style.display = "block";
+                document.getElementById('j1').innerHTML = scoreJ1/5;
 
             }else if (win == true && game.balle.position.z >0) {
 
-                // let i = 0;
-                // const interval = setInterval(() => {
-                //     if (i <= 1000) {
-                //         particlesJ1.particles[i].fart();
-                //         i++;
-                //     } else clearInterval(interval);
-                // }, 10);
+                document.getElementById('resultat').innerHTML = "Dans le mille !";
+                document.getElementById("imgResultat").src="img/best.png";
+                resultat.style.display = "block";
+                const timeout2 = setTimeout(() => {
+                    document.getElementById('resultat').innerHTML = "";
+                    resultat.style.display = "none";
+                }, 2500);
+                scoreJ2++;
+                if (scoreJ2 == 5) modal.style.display = "block";
+                document.getElementById('j2').innerHTML = scoreJ2/5;
+
+            }else {
+
+                document.getElementById('resultat').innerHTML = "Oups, raté...";
+                document.getElementById("imgResultat").src="img/sad.png";
+                resultat.style.display = "block";
+                const timeout2 = setTimeout(() => {
+                    document.getElementById('resultat').innerHTML = "";
+                    resultat.style.display = "none";
+                }, 2500);
 
             }
 
@@ -120,36 +211,22 @@ const handleClick = e => {
 
 
 
-        const timeout2 = setTimeout(() => {
+        const timeout3 = setTimeout(() => {
 
-            if (win==true) {
-                if (game.balle.position.z <0) {
-                    TweenMax.to(game.balle.position, 1.5, {
-                        x: 0,
-                        y: game.hauteurBalle,
-                        z: -game.z,
-                    });
-                    // TweenMax.to(camera.position, 2, {
-                    // 	x: '15',
-                    // 	y: '30',
-                    // 	z: `-95`,
-                    // });
-                    // game.balle.position.set(0,game.hauteurBalle,-game.z);
-                    direction='+';
-                }else {
-                    TweenMax.to(game.balle.position, 1.5, {
-                        x: 0,
-                        y: game.hauteurBalle,
-                        z: game.z,
-                    });
-                    // TweenMax.to(camera.position, 2, {
-                    // 	x: '-15',
-                    // 	y: '30',
-                    // 	z: `95`,
-                    // });
-                    // game.balle.position.set(0,game.hauteurBalle,game.z);
-                    direction='-';
-                }
+            if (game.balle.position.z <0) {
+                TweenMax.to(game.balle.position, 1.5, {
+                    x: 0,
+                    y: game.hauteurBalle,
+                    z: -game.z,
+                });
+                direction='+';
+            }else {
+                TweenMax.to(game.balle.position, 1.5, {
+                    x: 0,
+                    y: game.hauteurBalle,
+                    z: game.z,
+                });
+                direction='-';
             }
 
             touchable = true;
@@ -160,8 +237,44 @@ const handleClick = e => {
 };
 addEventListener('click', handleClick);
 
+// post processing
+const composer = new THREE.EffectComposer(renderer);
+
+const renderPass = new THREE.RenderPass(scene, camera);
+const grainPass = new THREE.ShaderPass(THREE.FilmShader);
+grainPass.renderToScreen = true;
+grainPass.uniforms['grayscale'] = false;
+
+composer.addPass(renderPass);
+composer.addPass(grainPass);
+// Fin de composer
+
 let nb = 50;
+let animation = true;
+let hsl = 0;
 const animate = timestamp => {
+
+    if (game.balle.position.z == game.z) {
+        document.getElementById('indicationJ1').innerHTML = 'À ton tour';
+        document.getElementById('indicationJ2').innerHTML = 'En attente';
+        document.getElementById("imgJ1").src="img/bottleCap.png";
+        document.getElementById("imgJ2").src="img/wait.png";
+    }else if (game.balle.position.z == -game.z) {
+        document.getElementById('indicationJ2').innerHTML = 'À ton tour';
+        document.getElementById('indicationJ1').innerHTML = 'En attente';
+        document.getElementById("imgJ1").src="img/wait.png";
+        document.getElementById("imgJ2").src="img/bottleCap.png";
+    }
+    if (hsl <=360) {
+        document.querySelectorAll('.score').forEach(c => {
+            c.style.backgroundColor = `hsl(${hsl},30%,60%)`;
+        });
+        modal.style.backgroundColor = `hsl(${hsl},50%,80%)`;
+        span.style.color = `hsl(${hsl},50%,80%)`;
+        hsl+=0.5;
+    }else {
+        hsl = 0;
+    }
 
     if (nb <= 0) {
         game.geometryContenuVerre.vertices.forEach(v => {
@@ -171,10 +284,17 @@ const animate = timestamp => {
         nb = 50;
     }
     nb --;
-    stats.begin();
 
-    renderer.render(scene, camera);
-    stats.end();
+    if (animation==true) {
+        // timer();
+        animation = false
+    }
+
+    // stats.begin();
+
+    // renderer.render(scene, camera);
+    composer.render();
+    // stats.end();
     requestAnimationFrame(animate);
 };
 animate();
